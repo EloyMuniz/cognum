80d599432711f3fd9159485b81d146db1f54160c
Eloy <eloymjunior00@gmail.com>
2023-10-03 22:38:56 -0300

    Alterações gerais

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
@@ -8,17 +8,7 @@ const HOST = "0.0.0.0";
 //Criação da inicialização do servidor
 const app = express();
 
-// // Middleware para tratar como Buffer Bruto apenas em uma rota específica
-// app.use((req, res, next) => {
-//   if (req.path === "/v1/stripe-webhook") {
-//     express.raw({ type: "application/json" })(req, res, next);
-//   } else {
-//     express.json()(req, res, next);
-//   }
-// });
-
 app.use(cors());
 app.use(routes);
 
 app.listen(PORT, HOST);
-
